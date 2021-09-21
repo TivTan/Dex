8d22bb2a14b8265ccdffb2e26e4a495772b428be
tivtan <tivtan@tivtan.com>
2021-09-21 21:00:05 +0800

    Upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
-import { Symfoni } from "./hardhat/SymfoniContext";
+import { Symfoni, CurrentAddressContext } from "./hardhat/SymfoniContext";
 import { Swap } from "./components/Swap";
 
 
 function App() {
 
+
+  
+  const [currentAddress] = useContext(CurrentAddressContext);
+
   return (
     <div className="App">
       <Symfoni autoInit={true} >
-        <div className="min-h-screen bg-gray-500">
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8" >
-            <div className="text-gray-100 text-6xl pt-28 pb-10">
+        <div className="min-h-screen bg-gray-500 font-sans border-box">
+          <div className="max-w-5xl mx-auto sm:px-6 lg:px-8" >                
+            <div className="text-gray-100 text-6xl pt-28 pb-10 border-1px">
               deX $wap
             </div>
             <Swap
@@ -20,7 +24,7 @@ function App() {
             ></Swap>
           </div>
         </div>
-      </Symfoni>    
+      </Symfoni>
     </div>
   );
 }
--- frontend/src/components/Swap.tsx
+++ frontend/src/components/Swap.tsx
@@ -10,85 +10,85 @@ interface Props {
 
 export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
     const ERC20Factory = useContext(ERC20Context);
-  
+
     const [tokenAInstance, setTokenAInstance] = useState<ERC20>();
     const [tokenBInstance, setTokenBInstance] = useState<ERC20>();
-  
+
     const [tokenASymbol, setTokenASymbol] = useState<string>();
     const [tokenBSymbol, setTokenBSymbol] = useState<string>();
-  
+
     useEffect(() => {
-      if (ERC20Factory.instance) {
-        setTokenAInstance(ERC20Factory.instance!.attach(tokenA));
-        setTokenBInstance(ERC20Factory.instance!.attach(tokenB));
-      }
-    }, [ERC20Factory.instance, tokenA, tokenB]);
-  
-    useEffect(() => {
-      const fetchTokenSymbols = async () => {
-        if (!tokenAInstance || !tokenBInstance)  {
-          return;
+        if (ERC20Factory.instance) {
+            setTokenAInstance(ERC20Factory.instance!.attach(tokenA));
+            setTokenBInstance(ERC20Factory.instance!.attach(tokenB));
         }
-        
-        setTokenASymbol(await tokenAInstance.symbol());
-        setTokenBSymbol(await tokenBInstance.symbol());
-      };
-      fetchTokenSymbols();
+    }, [ERC20Factory.instance, tokenA, tokenB]);
+
+    useEffect(() => {
+        const fetchTokenSymbols = async () => {
+            if (!tokenAInstance || !tokenBInstance) {
+                return;
+            }
+
+            setTokenASymbol(await tokenAInstance.symbol());
+            setTokenBSymbol(await tokenBInstance.symbol());
+        };
+        fetchTokenSymbols();
     }, [tokenAInstance, tokenBInstance])
-  
+
     const [amount, setAmount] = useState<number>(0);
-  
+
     const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setAmount(parseInt(event.target.value));
+        setAmount(parseInt(event.target.value));
     };
-  
+
     const router = useContext(UniswapV2Router02Context);
     const [exchangeAmount, setExchangeAmount] = useState<string>("0");
-  
+
     useEffect(() => {
-      const fetchExchangeAmount = async () => {
-        if (!router.instance) {
-          console.log("router instance not found");
-          return;
-        }
-  
-        if (amount > 0) {
-          // router gets angry if you pass in a 0
-          const amountsOut = await router.instance.getAmountsOut(
-            ethers.utils.parseEther(amount.toString()),
-            [tokenA, tokenB]
-          );
-          setExchangeAmount(ethers.utils.formatUnits(amountsOut[1].toString(), 18));
-        }
-      };
-  
-      fetchExchangeAmount();
+        const fetchExchangeAmount = async () => {
+            if (!router.instance) {
+                console.log("router instance not found");
+                return;
+            }
+
+            if (amount > 0) {
+                // router gets angry if you pass in a 0
+                const amountsOut = await router.instance.getAmountsOut(
+                    ethers.utils.parseEther(amount.toString()),
+                    [tokenA, tokenB]
+                );
+                setExchangeAmount(ethers.utils.formatUnits(amountsOut[1].toString(), 18));
+            }
+        };
+
+        fetchExchangeAmount();
     }, [router.instance, amount, tokenA, tokenB]);
-  
+
     const [currentAddress] = useContext(CurrentAddressContext);
-  
+
     const handleSwap = async () => {
-      if (!router.instance || !tokenAInstance) {
-        console.log("router or token instance not found");
-        return;
-      }
-      const time = Math.floor(Date.now() / 1000) + 3600;
-  
-      await (
-        await tokenAInstance.approve(
-          router.instance.address,
-          ethers.utils.parseEther(amount.toString())
-        )
-      ).wait();
-      await (
-        await router.instance.swapExactTokensForTokens(
-          ethers.utils.parseEther(amount.toString()),
-          0, // we shouldn't leave this as 0, it is dangerous in real trading
-          [tokenA, tokenB],
-          currentAddress,
-          time
-        )
-      ).wait();
+        if (!router.instance || !tokenAInstance) {
+            console.log("router or token instance not found");
+            return;
+        }
+        const time = Math.floor(Date.now() / 1000) + 3600;
+
+        await (
+            await tokenAInstance.approve(
+                router.instance.address,
+                ethers.utils.parseEther(amount.toString())
+            )
+        ).wait();
+        await (
+            await router.instance.swapExactTokensForTokens(
+                ethers.utils.parseEther(amount.toString()),
+                0, // we shouldn't leave this as 0, it is dangerous in real trading
+                [tokenA, tokenB],
+                currentAddress,
+                time
+            )
+        ).wait();
     };
 
     let z = Number(exchangeAmount) / Number(amount);
@@ -98,19 +98,27 @@ export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
     return (
         <div>
             <div className="bg-red-400 p-6 shadow sm:rounded-lg">
+
+                <div className="p-2 bg-red-200 shadow sm:rounded-lg">
+                    <div className="w-full">
+                        <div className="p-0">
+                            <label className="text-gray-800 text-2xl"> Connected Address:    </label>
+                            <label className="text-gray-800 text-2xl"> {currentAddress} </label>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="w-full">
-                    <div className="p-1">
+                    <div className="p-5">
                         <label className="p-5 text-gray-800 text-3xl"> {tokenASymbol}</label>
                         <label className="p-5 text-gray-800 text-3xl"> {'--->'} </label>
                         <label className="p-5 text-gray-800 text-3xl">{tokenBSymbol}</label>
                         <label className="p-5 text-gray-800 text-3xl">Amount: </label>
                         <input className="w-20 rounded"
-                                type="number"
-                                
-                                min="1"
-                                step="0.01"
-                                placeholder="0.01"
-                                onChange={handleAmountChange}
+                            type="number"
+                            step="0.1"                            
+                            placeholder="> 0"
+                            onChange={handleAmountChange}
                         />
 
                         <label className="p-3 text-gray-800 text-3xl">Receive:</label>
@@ -118,7 +126,7 @@ export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
                             type="text"
                             name="Receive"
                             id="receive"
-                            className="w-28 rounded"
+                            className="w-32 rounded"
                             value={exchangeAmount}
                         />
 
@@ -133,21 +141,14 @@ export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
                     </div>
                 </div>
 
-                <div className="p-2 bg-red-500 shadow sm:rounded-lg">
-                    <div className="w-full">
-                        <div className="p-1">
-                            <label className="text-gray-800 text-2xl"> Connected Address: </label>
-                            <label className="text-gray-800 text-2xl"> {currentAddress} </label>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="p-2 bg-black shadow sm:rounded-lg">
+
+                <div className="inline-flex items-center justify-center px-3 py-2 border p-2 bg-black sm:rounded-lg">
                     <div className="w-full">
-                        <div className="p-1">
-                            <label className="text-white text-2xl"> Exchange Rate: </label>
-                            <label className="text-white text-2xl"> {rate} </label>
-                        </div>
+                        
+                            <label className="text-white text-xl"> Exchange Rate :   </label>
+                            <label className="text-white text-xl">   {rate} </label>
+                        
                     </div>
                 </div>
 
