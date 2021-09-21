8cf1672b2589732264493cdb5670e1486348be13
tivtan <tivtan@tivtan.com>
2021-09-21 21:48:29 +0800

    Upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
-import { Symfoni, CurrentAddressContext } from "./hardhat/SymfoniContext";
+import { Symfoni  } from "./hardhat/SymfoniContext";
 import { Swap } from "./components/Swap";
 
 
